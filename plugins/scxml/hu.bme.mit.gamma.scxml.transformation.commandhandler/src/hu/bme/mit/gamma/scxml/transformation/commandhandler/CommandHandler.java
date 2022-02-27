ca046f3ce43eda9b5691fcfe4218a2569f8f771e
Bálint Radnai <radnai.balint@edu.bme.hu>
2022-02-27 15:29:44 +0100

    Initialize SCXML transformation projects

---
+++ plugins/scxml/hu.bme.mit.gamma.scxml.transformation.commandhandler/src/hu/bme/mit/gamma/scxml/transformation/commandhandler/CommandHandler.java
@@ -0,0 +1,15 @@
+package hu.bme.mit.gamma.scxml.transformation.commandhandler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class CommandHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
