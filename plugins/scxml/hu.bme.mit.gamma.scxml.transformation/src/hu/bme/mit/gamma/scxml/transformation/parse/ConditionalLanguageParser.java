5e9a83e6505d31b8a77905c65681f34b4c7eeee5
Bálint Radnai <radnai.balint@edu.bme.hu>
2022-03-30 13:20:17 +0200

    Remove unnecessary parser related classes

--- plugins/scxml/hu.bme.mit.gamma.scxml.transformation/src/hu/bme/mit/gamma/scxml/transformation/parse/ConditionalLanguageParser.java
+++ plugins/scxml/hu.bme.mit.gamma.scxml.transformation/src/hu/bme/mit/gamma/scxml/transformation/parse/ConditionalLanguageParser.java
@@ -19,49 +19,44 @@ import hu.bme.mit.gamma.expression.model.ExpressionModelFactory;
 import hu.bme.mit.gamma.expression.model.OpaqueExpression;
 
 public class ConditionalLanguageParser {
-	
+
 	ExpressionModelFactory expressionModelFactory = ExpressionModelFactory.eINSTANCE;
 	
-	//private Injector injector = new CustomLanguageStandaloneSetup()
-	//						.createInjectorAndDoEMFRegistration();
-	private Injector injector = new ExpressionLanguageStandaloneSetup()
-			.createInjectorAndDoEMFRegistration();
-	
+	private Injector injector = new ExpressionLanguageStandaloneSetup().createInjectorAndDoEMFRegistration();
+
 	public Expression parse(String expression, Map<String, Declaration> scope) {
 		CustomLanguageParser parser = injector.getInstance(CustomLanguageParser.class);
-		 StringReader reader = new StringReader(expression);
-		 IParseResult result = parser.parse(reader);
-		 
-		 if (result.hasSyntaxErrors()) {
-			 return createOpaqueExpression(expression);
-		 }
-		 
-		 try {
-			 for (INode node : result.getRootNode().getLeafNodes()) {
-				 if (node.getGrammarElement() instanceof CrossReference) {
-					 final DirectReferenceExpression refExpr =
-							 (DirectReferenceExpression) node.getSemanticElement();
-					 
-					 Declaration decl = scope.get(node.getText());
-					 if (decl != null) {
-						 refExpr.setDeclaration(decl);
-					 } else {
-						 throw new NoSuchElementException();
-					 }
-				 }
-			 }
-			 return (Expression) result.getRootASTElement();
-		 }
-		 catch (NoSuchElementException e) {
-		 }
-		 
-		 return createOpaqueExpression(expression);
+		StringReader reader = new StringReader(expression);
+		IParseResult result = parser.parse(reader);
+
+		if (result.hasSyntaxErrors()) {
+			return createOpaqueExpression(expression);
+		}
+
+		try {
+			for (INode node : result.getRootNode().getLeafNodes()) {
+				if (node.getGrammarElement() instanceof CrossReference) {
+					final DirectReferenceExpression refExpr = (DirectReferenceExpression) node.getSemanticElement();
+
+					Declaration decl = scope.get(node.getText());
+					if (decl != null) {
+						refExpr.setDeclaration(decl);
+					} else {
+						throw new NoSuchElementException();
+					}
+				}
+			}
+			return (Expression) result.getRootASTElement();
+		} catch (NoSuchElementException e) {
+		}
+
+		return createOpaqueExpression(expression);
 	}
-	
+
 	private Expression createOpaqueExpression(String expression) {
 		OpaqueExpression opaqueExpression = expressionModelFactory.createOpaqueExpression();
 		opaqueExpression.setExpression(expression);
 		return opaqueExpression;
 	}
-	
+
 }
